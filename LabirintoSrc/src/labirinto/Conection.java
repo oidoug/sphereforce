bacc56007f4278f695e8dc1874576e43a93b30f4
mefistofiles <mefistofiles@2cba51e2-b130-0410-aa7c-d57a1e54fef9>
2007-05-17 22:36:23 +0000

    Todos os Arquivos de projeto do Jogo, incluindo a pasta-projeto do NetBeans, imagens e fontes do gimp (.xcf).

---
+++ LabirintoSrc/src/labirinto/Conection.java
@@ -0,0 +1,35 @@
+/*
+ * Conection.java
+ *
+ * Created on April 22, 2007, 11:46 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package labirinto;
+
+import java.net.Socket;
+
+/**
+ *
+ * @author das
+ */
+public class Conection {
+    
+    private Socket server;
+    private Socket client;
+    
+    /** Creates a new instance of Conection */
+    public Conection() {
+    }
+    
+    public void Send(int x, int y) {
+        
+    }
+    
+    public void Receive(int x, int y) {
+        
+    }
+    
+}
