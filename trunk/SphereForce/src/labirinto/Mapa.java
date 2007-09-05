f0ec50c2e4c748c5999901b027fc9a157fa74ca2
douglaslondrina <douglaslondrina@2cba51e2-b130-0410-aa7c-d57a1e54fef9>
2007-09-05 20:41:20 +0000

    background buracos - pau parede - sei la

--- trunk/SphereForce/src/labirinto/Mapa.java
+++ trunk/SphereForce/src/labirinto/Mapa.java
@@ -9,11 +9,13 @@
 
 package labirinto;
 
+import java.awt.Graphics;
 import java.awt.Image;
 import java.util.LinkedList;
 import labirinto.core.Buraco;
 import labirinto.core.Esfera;
 import labirinto.core.Marca;
+import labirinto.core.Parede;
 
 /**
  *
@@ -21,11 +23,15 @@ import labirinto.core.Marca;
  */
 public class Mapa {
 
-    private LinkedList paredes;
-    private LinkedList buracos;
+    private LinkedList<Parede> paredes;
+    private LinkedList<Buraco> buracos;
 
-    private Image buraco;
-    private Image bloco;
+    private Image buracoImg;
+    private Image blocoImg;
+    private Image backgroundImg;
+
+    private final int BackGW = 50;
+    private final int BackGH = 50;
 
     private Marca largada;
     private Marca chegada;
@@ -36,6 +42,8 @@ public class Mapa {
     private final int NUM_BURACOS = 20;
 
     public Mapa() {
+        buracos = new LinkedList<Buraco>();
+        paredes = new LinkedList<Parede>();
     }
 
     public void addObject(Object object) {
@@ -52,28 +60,72 @@ public class Mapa {
                 largada = (Marca) object;
             }
         } else if (object instanceof Image) {
-            if (buraco != null) {
-                bloco = (Image) object;
+            if (buracoImg == null) {
+                buracoImg = (Image) object;
+            } else if (blocoImg == null) {
+                blocoImg = (Image) object;
             } else {
-                buraco = (Image) object;
+                backgroundImg = (Image) object;
             }
         } else {
             System.err.println("erro: Objeto nao valido.");
         }
     }
-
-    public void refresh() {
-        
+    
+    public void gerarMapa() {
+        gerarBuracos();
+        gerarParedes();
     }
 
-    @SuppressWarnings(value = "unchecked")
-    public void gerarBuracos() {
+    public void paint(Graphics g) {
+        /** BEGIN background */
+        int x = 0;
+        int y = 0;
+
+        while (y < Main.WINDOW_HEIGHT) {
+            x = 0;
+            while (x < Main.WINDOW_WIDTH) {
+                g.drawImage(backgroundImg, x, y, null);
+                x = x + BackGW;
+            }
+            y = y + BackGH;
+        }
+        /* END background */
+
+        /* BEGIN buracos */
+        for (Buraco hole : buracos) {
+            hole.paint(g);
+        }
+        /* END buracos */
+
+        /* BEGIN paredes */
+        for (Parede wall : paredes) {
+            wall.paint(g);
+        }
+        /* END paredes */
+    }
+
+    private void gerarBuracos() {
         int randX;
         int randY;
         for (int i = 0; i <= NUM_BURACOS; i++) {
             randX = (int) Math.random();
             randY = (int) Math.random();
-            buracos.add(new Buraco(buraco, randX, randY));
+
+            Buraco hole = new Buraco(buracoImg, randX, randY);
+
+            if (!hole.colideCom(chegada) || !hole.colideCom(largada)) {
+                for (Parede parede : paredes) {
+                    if (!hole.colideCom(parede)) {
+                        buracos.add(new Buraco(buracoImg, randX, randY));
+                    }
+                }
+            }
         }
+    }
+
+    private void gerarParedes() {
+        // a dona aranha, subui pela parede, venho a chuva forte e um granito atravessou seu crânio
+        paredes.add(new Parede(blocoImg, 5, true, 400, 100));
     }
 }
