b41a912b73df5b034e8aaf9d8c9000a1d4a4042e
douglaslondrina <douglaslondrina@2cba51e2-b130-0410-aa7c-d57a1e54fef9>
2007-09-06 02:20:42 +0000

    geraçao de buracos +- geraçao e pintura paredes ok

--- SphereForce/src/labirinto/core/Parede.java
+++ SphereForce/src/labirinto/core/Parede.java
@@ -23,14 +23,14 @@ public class Parede {
     private float y;
     // tamanho referece a quantidade de blocos que compoe a parede
     private int tamanho;
-    
+
     // tamanho absoluto em pixels
     private int absTamanhoW;
     private int absTamanhoH;
-    
+
     // imagem do bloco
     private Image blocoimage;
-    
+
     //tamanho das laterias de cada bloco
     private int blocoH;
     private int blocoW;
@@ -44,12 +44,12 @@ public class Parede {
         this.vertical = vertical;
         this.x = x;
         this.y = y;
-        
+
         //colocar valores corretos
         blocoH = 20;
         blocoW = 20;
-        
-        if(vertical) {
+
+        if (vertical) {
             absTamanhoW = tamanho * blocoW;
             absTamanhoH = blocoH;
         } else {
@@ -58,22 +58,22 @@ public class Parede {
         }
     }
 
-    public int getH(){
+    public int getH() {
         return blocoH;
     }
-    
-    public int getW(){
+
+    public int getW() {
         return blocoW;
     }
-    
+
     public int getAbsTamanhoW() {
         return absTamanhoW;
     }
-    
+
     public int getAbsTamanhoH() {
-        return absTamanhoH;                
+        return absTamanhoH;
     }
-    
+
     public int getTamanho() {
         return tamanho;
     }
@@ -105,47 +105,42 @@ public class Parede {
     public void setY(float y) {
         this.y = y;
     }
-    
+
     // pinta parede na tela
     public void paint(Graphics g) {
-        
-    }
-    
-    public void colideCom(Esfera esfera) {
-        
-        if ((esfera.getY() > y - esfera.getRaio()) &&
-                (esfera.getY() < y + absTamanhoH + esfera.getRaio()) ) {
-           
-            //verifica se colide na parte lateral esquerda da parede
-            if ( (esfera.getX() - x) < (esfera.getX() + 2*esfera.getRaio()) ){
-                esfera.setVelX(-esfera.getVelX());
-                esfera.setX(x+2*esfera.getRaio());
+        for (int i = 0; i < tamanho; i++) {
+            if (vertical) {
+                g.drawImage(blocoimage,(int) x + (i * blocoH),(int) y, null);
+            } else {
+                g.drawImage(blocoimage,(int) x,(int) y + (i * blocoW), null);
             }
-            
-            //colisao com o lado direito da parede
-            else if ((x + absTamanhoW + esfera.getRaio()) > esfera.getX()){
+        }
+    }
+
+    public void colideCom(Esfera esfera) {
+
+        if ((esfera.getY() > y - esfera.getRaio()) && (esfera.getY() < y + absTamanhoH + esfera.getRaio())) {
+
+            //verifica se colide na parte lateral esquerda da parede
+            if ((esfera.getX() - x) < (esfera.getX() + 2 * esfera.getRaio())) {
+                esfera.setVelX(-esfera.getVelX());
+                esfera.setX(x + 2 * esfera.getRaio());
+            } else if ((x + absTamanhoW + esfera.getRaio()) > esfera.getX()) {
                 esfera.setVelX(-esfera.getVelX());
                 esfera.setX(x);
             }
         }
-        
-        if ((esfera.getX() > x - esfera.getRaio()) &&
-                (esfera.getX() < x + absTamanhoW + esfera.getRaio()) ) {
-            
+
+        if ((esfera.getX() > x - esfera.getRaio()) && (esfera.getX() < x + absTamanhoW + esfera.getRaio())) {
+
             //colisao com a parte superior da parede
-            if ( (esfera.getY() - y) < (esfera.getY() + 2*esfera.getRaio()) ) {
+            if ((esfera.getY() - y) < (esfera.getY() + 2 * esfera.getRaio())) {
                 esfera.setVelY(-esfera.getVelY());
-                esfera.setY(y - 2*esfera.getRaio());
-            }
-            
-            //colisao com a parte inferior da parede
-            else if ((y + absTamanhoH + esfera.getRaio()) > esfera.getY()) {
+                esfera.setY(y - 2 * esfera.getRaio());
+            } else if ((y + absTamanhoH + esfera.getRaio()) > esfera.getY()) {
                 esfera.setVelY(-esfera.getVelY());
                 esfera.setY(y);
             }
         }
-        
-        
-        
     }
 }
